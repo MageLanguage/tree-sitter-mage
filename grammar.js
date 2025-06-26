3b1cc9dc3dcb1fa6ef9fc4cff1d3d7f133c5346b
Alexander Kosachev <kosachev_alex@mail.ru>
2025-06-26 19:45:05 +0300

    v0.0.1

--- grammar.js
+++ grammar.js
@@ -10,8 +10,56 @@
 module.exports = grammar({
   name: "mage",
 
+  conflicts: ($) => [[$.definition]],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
-  }
+    source_file: ($) => optional($.statement_chain),
+    source: ($) => seq("{", optional($.statement_chain), "}"),
+
+    string: () => /"[^"]*"/,
+
+    zero: () => "0",
+    binary: () => /0[Bb][0-1]+/,
+    octal: () => /0[Oo][0-7]+/,
+    decimal: () => /0[Dd][0-9]+/,
+    hex: () => /0[Xx][0-9A-Fa-f]+/,
+
+    number: ($) => choice($.zero, $.binary, $.octal, $.decimal, $.hex),
+
+    math_operation: ($) => choice("+", "-", "*", "/", "%"),
+    math: ($) => {
+      let variable = choice($.identifier_chain, $.number);
+
+      return seq(
+        "[",
+        optional(variable),
+        repeat(seq($.math_operation, variable)),
+        "]",
+      );
+    },
+
+    name: () => /\w+/,
+    call: ($) =>
+      seq(
+        $.identifier,
+        "(",
+        optional(seq($.statement, repeat(seq(",", $.statement)))),
+        ")",
+      ),
+
+    identifier_chain: ($) => seq($.identifier, repeat(seq(".", $.identifier))),
+    identifier: ($) => choice($.name, $.call),
+
+    definition_operation: ($) => choice(":", "="),
+    definition: ($) =>
+      seq(
+        repeat1(seq($.identifier_chain, $.definition_operation)),
+        $.statement,
+      ),
+
+    statement_chain: ($) =>
+      seq($.statement, repeat(seq(";", $.statement)), optional(";")),
+    statement: ($) =>
+      choice($.definition, $.identifier_chain, $.math, $.string, $.source),
+  },
 });

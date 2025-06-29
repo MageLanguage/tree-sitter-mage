40deacc38ed4138fc4e3d384903569fd5eff0ba6
Alexander Kosachev <kosachev_alex@mail.ru>
2025-06-29 20:47:19 +0300

    v0.0.5

--- grammar.js
+++ grammar.js
@@ -24,41 +24,43 @@ module.exports = grammar({
 
     number: ($) => choice($.zero, $.binary, $.octal, $.decimal, $.hex),
 
-    math_operation: ($) => choice("+", "-", "*", "/", "%"),
-    math: ($) => {
-      let variable = choice($.identifier_chain, $.math, $.number);
-
-      return seq(
-        "[",
-        optional(variable),
-        repeat(seq($.math_operation, variable)),
-        "]",
-      );
-    },
-
     name: () => /\w+/,
     call: ($) =>
       seq(
         $.identifier,
         "(",
-        optional(seq($.statement, repeat(seq(",", $.statement)))),
+        optional(seq($.expression, repeat(seq(",", $.expression)))),
         ")",
       ),
 
     identifier_chain: ($) => seq($.identifier, repeat(seq(".", $.identifier))),
     identifier: ($) => choice($.name, $.call),
 
-    expression: ($) => choice($.identifier_chain, $.math, $.string, $.source),
+    definition: () => choice(":", "="),
 
-    definition_operation: ($) => choice(":", "="),
-    definition: ($) =>
-      seq(
-        repeat1(seq($.identifier_chain, $.definition_operation)),
-        $.expression,
-      ),
+    arithmetic: () => choice("+", "-", "*", "/", "%"),
+
+    expression: ($) => {
+      let variable = choice(
+        $.prioritize,
+        $.identifier_chain,
+        $.number,
+        $.string,
+        $.source,
+      );
+
+      return seq(
+        repeat($.arithmetic),
+        variable,
+        repeat(seq(repeat1($.arithmetic), variable)),
+      );
+    },
+
+    prioritize: ($) => seq("[", $.expression, "]"),
 
     statement_chain: ($) =>
       seq($.statement, repeat(seq(";", $.statement)), optional(";")),
-    statement: ($) => choice($.definition, $.expression),
+    statement: ($) =>
+      seq(repeat(seq($.identifier_chain, $.definition)), $.expression),
   },
 });
